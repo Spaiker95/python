51bcc70617c1d1bf13079624a818151f8dbf006c
Spaiker95 <klyuchnikov_andrei@mail.ru>
2024-12-25 22:52:11 +0300

    my third commit

--- myfile.py
+++ myfile.py
@@ -1,5 +1,15 @@
-for i in range(11):
-    print("hello")
-    print("my changes")
+class My_class:
+    def __init__(self, x = 1, y = 1):
+        self.x = x
+        self.y = y
+
+    def summ(self):
+        return self.x + self.y
+    
+    
+tom = My_class(4, 3)
+
+print(tom.summ())
     
 
+        
